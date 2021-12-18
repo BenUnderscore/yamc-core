0018571ffdd2106da18378c68925defd40cab107
Benjamin Kalatsjan-Kirejevas <bensujaam@gmail.com>
2021-12-18 21:51:49 +0100

    Added bare-bones window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+
+use glium::glutin::event::{Event, WindowEvent};
+use glium::glutin::event_loop::ControlFlow;
+
 fn main() {
-    println!("Hello, world!");
+    let mut events_loop = glium::glutin::event_loop::EventLoop::new();
+    let wb = glium::glutin::window::WindowBuilder::new()
+        .with_inner_size(glium::glutin::dpi::LogicalSize::new(1024.0, 768.0))
+        .with_title("Hello world");
+    let cb = glium::glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &events_loop).unwrap();
+    events_loop.run(
+        |ev, _target, control_flow| {
+            match ev
+            {
+                Event::WindowEvent { window_id, event } =>
+                    match event {
+                        WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                        _ => (),
+                    },
+                _ => (),
+            };
+        }
+    );
 }
