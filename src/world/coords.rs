fe47e7dc651a57db8d255dcad120f6f923feeacb
Benjamin Kalatsjan-Kirejevas <bensujaam@gmail.com>
2022-01-21 17:46:21 +0200

    Implemented coordinate utilities

--- src/world/coords.rs
+++ src/world/coords.rs
@@ -1,9 +1,9 @@
 //! This module has utilities for converting between different coordinate frames.
 //!
 //! In the YAMC engine there are three types of voxel coordinates:
-//! - Chunk-local voxel coordinates (Local coordinates)
-//! - Global chunk coordinates (Chunk coordinates)
-//! - Global voxel coordinates (Global coordinates)
+//! - Chunk-local voxel coordinates (Local coordinates) (`u32`)
+//! - Global chunk coordinates (Chunk coordinates) (`i32`)
+//! - Global voxel coordinates (Global coordinates) (`i32`)
 //!
 //! Local coordinates index voxels within a given chunk and are relative to its origin.
 //! They are used to directly index into the voxel buffer, but are useless outside of the context of a chunk.
@@ -14,15 +14,36 @@
 //! Because this can be cumbersome and a higher level abstraction is required for certain gameplay logic,
 //! global coordinates are simply voxel coordinates that are relative to the world origin.
 //!
-//! All coordinates should use signed 32-bit numbers (`i32`) since they are in the sweet spot of being large
-//! (4 billion x 4 billion voxels for global coordinates) and small.
-//!
 //! When passing coordinates to functions, they should be passed with all components being separate.
 //! This makes it easier to do immediate math on the numbers, since they do not need to be
 //! accessed with a dot operator every time.
 //! When there are multiple sets of coordinates passed to a function, it is okay to pass them as tuples
 //! and to call them `xyz_*`
 
-pub fn local_to_global(xyz_local: (i32, i32, i32), xyz_chunk: (i32, i32, i32)) -> (i32, i32, i32) {
-    unimplemented!()
+//Uses
+use super::chunk;
+
+/// Takes a combination of local coordinates and chunk coordinates and returns the global coordinates for it
+pub fn local_to_global(xyz_local: (u32, u32, u32), xyz_chunk: (i32, i32, i32)) -> (i32, i32, i32) {
+    (
+        xyz_local.0 as i32 + xyz_chunk.0 * chunk::CHUNK_SIZE_X as i32,
+        xyz_local.1 as i32 + xyz_chunk.1 * chunk::CHUNK_SIZE_X as i32,
+        xyz_local.2 as i32 + xyz_chunk.2 * chunk::CHUNK_SIZE_X as i32
+    )
+}
+
+pub fn global_to_local(x: i32, y: i32, z: i32) -> ((u32, u32, u32), (i32, i32, i32)) {
+    let chunk_coords = (
+        x.div_euclid(chunk::CHUNK_SIZE_X as i32),
+        y.div_euclid(chunk::CHUNK_SIZE_Y as i32),
+        z.div_euclid(chunk::CHUNK_SIZE_Z as i32)
+    );
+
+    let local_coords = (
+        x.rem_euclid(chunk::CHUNK_SIZE_X as i32) as u32,
+        y.div_euclid(chunk::CHUNK_SIZE_Y as i32) as u32,
+        z.div_euclid(chunk::CHUNK_SIZE_Z as i32) as u32
+    );
+
+    (local_coords, chunk_coords)
 }
